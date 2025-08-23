2016f1968bce08159710d172f447daffbb671ed3
SHIVAM KUMAR JHA <shivamjha082001@gmail.com>
2025-08-23 16:00:14 +0530

    create: migration branch

--- backend/src/controllers/cartController.js
+++ backend/src/controllers/cartController.js
@@ -26,7 +26,7 @@ const getCart = async (req, res) => {
         }]
       }]
     });
-    //testing: migration branch
+    //testing: prod branch
     if (!cart) {
       // Create cart if it doesn't exist
       const newCart = await Cart.create({ user_id: req.user.id });
