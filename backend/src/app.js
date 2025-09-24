2d43ec628d429732565ecdf092664bf44215d713
SHIVAM KUMAR JHA <shivamjha082001@gmail.com>
2025-09-24 19:34:03 +0530

    paused rate limiting for dev purpose

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,11 +27,15 @@ app.use(cors({
 }));
 
 // Rate limiting
+
+/** 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100 // limit each IP to 100 requests per windowMs
 });
 app.use('/api', limiter);
+
+*/
 
 // General middleware
 app.use(compression());
