cebe84d437809829ba0d993363174b28456ac043
SHIVAM KUMAR JHA <shivamkumarjha0825@gmail.com>
2025-09-24 19:52:22 +0530

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,11 +30,15 @@ app.use(cors({
 
 
 /** 
+ * 
+ 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100 // limit each IP to 100 requests per windowMs
 });
 app.use('/api', limiter);
+
+
 
 */
 
